55be3abaa7c9543f4c111daf5162580d557fe1fb
Azlan Ainto <azlanainto97@gmail.com>
2026-01-10 23:29:52 +0100

    Tabellen: Produkt und Kategorie hinzugefügt.

--- mercanto.sql
+++ mercanto.sql
@@ -1,10 +1,21 @@
 --
--- File generated with SQLiteStudio v3.4.20 on Sa Jan 10 20:48:29 2026
+-- File generated with SQLiteStudio v3.4.20 on Sa Jan 10 23:26:32 2026
 --
 -- Text encoding used: System
 --
 PRAGMA foreign_keys = off;
 BEGIN TRANSACTION;
+
+-- Table: Kategorie
+DROP TABLE IF EXISTS Kategorie;
+
+CREATE TABLE IF NOT EXISTS Kategorie (
+    kategorieID  INTEGER    PRIMARY KEY AUTOINCREMENT
+                            NOT NULL,
+    titel        TEXT (255),
+    beschreibung TEXT (255) 
+);
+
 
 -- Table: Kunden
 DROP TABLE IF EXISTS Kunden;
@@ -21,6 +32,20 @@ CREATE TABLE IF NOT EXISTS Kunden (
     stadt         TEXT,
     Mobilenummer  TEXT,
     Telefonnummer TEXT
+);
+
+
+-- Table: Produkt
+DROP TABLE IF EXISTS Produkt;
+
+CREATE TABLE IF NOT EXISTS Produkt (
+    ProduktID     INTEGER    PRIMARY KEY AUTOINCREMENT
+                             NOT NULL,
+    ean           TEXT (255) UNIQUE,
+    preis         REAL,
+    bezeichnug    TEXT (255),
+    gewicht       REAL,
+    verfallsdatum TEXT
 );
 
 
