e0062d790edd3c236b9979e90f2a15fff74eb7b1
Azlan Ainto <azlanainto97@gmail.com>
2026-01-11 20:36:44 +0100

    Länge der Zeichen geändert.

--- mercanto.sql
+++ mercanto.sql
@@ -1,5 +1,5 @@
 --
--- File generated with SQLiteStudio v3.4.20 on So Jan 11 20:15:45 2026
+-- File generated with SQLiteStudio v3.4.20 on So Jan 11 20:36:00 2026
 --
 -- Text encoding used: System
 --
@@ -10,17 +10,17 @@ BEGIN TRANSACTION;
 DROP TABLE IF EXISTS Adresse;
 
 CREATE TABLE IF NOT EXISTS Adresse (
-    AdressID   INTEGER PRIMARY KEY AUTOINCREMENT
-                       NOT NULL,
-    KundeID    INTEGER REFERENCES Kunden (KundenID) ON DELETE CASCADE
-                                                    ON UPDATE CASCADE
-                       NOT NULL,
-    Strasse    TEXT    NOT NULL,
-    Hausnummer TEXT    NOT NULL,
-    PLZ        TEXT    NOT NULL,
-    Typ        TEXT    NOT NULL,
-    Stadt      TEXT,
-    Land       TEXT
+    AdressID   INTEGER    PRIMARY KEY AUTOINCREMENT
+                          NOT NULL,
+    KundeID    INTEGER    REFERENCES Kunden (KundenID) ON DELETE CASCADE
+                                                       ON UPDATE CASCADE
+                          NOT NULL,
+    Strasse    TEXT (255) NOT NULL,
+    Hausnummer TEXT       NOT NULL,
+    PLZ        TEXT (5)   NOT NULL,
+    Typ        TEXT (255) NOT NULL,
+    Stadt      TEXT (255),
+    Land       TEXT (255) 
 );
 
 
@@ -49,19 +49,19 @@ CREATE TABLE IF NOT EXISTS Bestellposition (
 DROP TABLE IF EXISTS Bestellung;
 
 CREATE TABLE IF NOT EXISTS Bestellung (
-    BestellungID     INTEGER PRIMARY KEY AUTOINCREMENT
-                             NOT NULL,
-    Datum            TEXT    NOT NULL,
-    KundenID         INTEGER REFERENCES Kunden (KundenID) ON DELETE CASCADE
-                                                          ON UPDATE CASCADE
-                             NOT NULL,
-    ProduktID        INTEGER REFERENCES Produkt (ProduktID) ON DELETE CASCADE
-                                                            ON UPDATE CASCADE
-                             NOT NULL,
-    MitarbeiterID    INTEGER REFERENCES Mitarbeiter (MitarbeiterID) ON DELETE CASCADE
-                                                                    ON UPDATE CASCADE,
-    Lieferdatum      TEXT    NOT NULL,
-    Status           TEXT,
+    BestellungID     INTEGER    PRIMARY KEY AUTOINCREMENT
+                                NOT NULL,
+    Datum            TEXT (10)  NOT NULL,
+    KundenID         INTEGER    REFERENCES Kunden (KundenID) ON DELETE CASCADE
+                                                             ON UPDATE CASCADE
+                                NOT NULL,
+    ProduktID        INTEGER    REFERENCES Produkt (ProduktID) ON DELETE CASCADE
+                                                               ON UPDATE CASCADE
+                                NOT NULL,
+    MitarbeiterID    INTEGER    REFERENCES Mitarbeiter (MitarbeiterID) ON DELETE CASCADE
+                                                                       ON UPDATE CASCADE,
+    Lieferdatum      TEXT       NOT NULL,
+    Status           TEXT (255),
     Gesamtbetrag     REAL,
     Lieferadresse    TEXT,
     Rechnungsadresse TEXT
@@ -83,17 +83,17 @@ CREATE TABLE IF NOT EXISTS Kategorie (
 DROP TABLE IF EXISTS Kunden;
 
 CREATE TABLE IF NOT EXISTS Kunden (
-    KundenID      INTEGER PRIMARY KEY AUTOINCREMENT
-                          NOT NULL,
-    vorname       TEXT    NOT NULL,
-    nachname      TEXT    NOT NULL,
-    geburtsdatum  TEXT    NOT NULL,
-    strasse       TEXT,
-    hausnummer    INTEGER,
-    plz           TEXT,
-    stadt         TEXT,
-    Mobilenummer  TEXT,
-    Telefonnummer TEXT
+    KundenID       INTEGER    PRIMARY KEY AUTOINCREMENT
+                              NOT NULL,
+    vorname        TEXT (155) NOT NULL,
+    nachname       TEXT (155) NOT NULL,
+    geburtsdatum10 TEXT (10)  NOT NULL,
+    strasse        TEXT (155),
+    hausnummer     INTEGER,
+    plz            TEXT (5),
+    stadt          TEXT (155),
+    Mobilenummer   TEXT (12),
+    Telefonnummer  TEXT (20) 
 );
 
 
@@ -113,12 +113,12 @@ CREATE TABLE IF NOT EXISTS Lieferant (
 DROP TABLE IF EXISTS Mitarbeiter;
 
 CREATE TABLE IF NOT EXISTS Mitarbeiter (
-    MitarbeiterID INTEGER PRIMARY KEY AUTOINCREMENT
-                          NOT NULL,
-    Vorname       TEXT    NOT NULL,
-    Nachname      TEXT    NOT NULL,
-    Beruf         TEXT,
-    Abteilung     TEXT
+    MitarbeiterID INTEGER    PRIMARY KEY AUTOINCREMENT
+                             NOT NULL,
+    Vorname       TEXT (155) NOT NULL,
+    Nachname      TEXT (155) NOT NULL,
+    Beruf         TEXT (155),
+    Abteilung     TEXT (155) 
 );
 
 
@@ -132,7 +132,7 @@ CREATE TABLE IF NOT EXISTS Produkt (
     preis         REAL,
     bezeichnug    TEXT (255),
     gewicht       REAL,
-    verfallsdatum TEXT
+    verfallsdatum TEXT (10) 
 );
 
 
