67beb999daaf41e76bd5d535e4c8798c5f5572ec
Azlan Ainto <azlanainto97@gmail.com>
2026-01-11 20:18:03 +0100

    Tabelle Produkt, Bestellung, Bestellposition, Miarbeiter, Adresse, Liefertant erstellt

--- mercanto.sql
+++ mercanto.sql
@@ -1,10 +1,72 @@
 --
--- File generated with SQLiteStudio v3.4.20 on Sa Jan 10 23:26:32 2026
+-- File generated with SQLiteStudio v3.4.20 on So Jan 11 20:15:45 2026
 --
 -- Text encoding used: System
 --
 PRAGMA foreign_keys = off;
 BEGIN TRANSACTION;
+
+-- Table: Adresse
+DROP TABLE IF EXISTS Adresse;
+
+CREATE TABLE IF NOT EXISTS Adresse (
+    AdressID   INTEGER PRIMARY KEY AUTOINCREMENT
+                       NOT NULL,
+    KundeID    INTEGER REFERENCES Kunden (KundenID) ON DELETE CASCADE
+                                                    ON UPDATE CASCADE
+                       NOT NULL,
+    Strasse    TEXT    NOT NULL,
+    Hausnummer TEXT    NOT NULL,
+    PLZ        TEXT    NOT NULL,
+    Typ        TEXT    NOT NULL,
+    Stadt      TEXT,
+    Land       TEXT
+);
+
+
+-- Table: Bestellposition
+DROP TABLE IF EXISTS Bestellposition;
+
+CREATE TABLE IF NOT EXISTS Bestellposition (
+    PositionsID  INTEGER PRIMARY KEY AUTOINCREMENT
+                         NOT NULL,
+    BestellungID INTEGER REFERENCES Bestellung (BestellungID) ON DELETE CASCADE
+                                                              ON UPDATE CASCADE
+                         NOT NULL,
+    ProduktID    INTEGER REFERENCES Produkt (ProduktID) ON DELETE CASCADE
+                                                        ON UPDATE CASCADE
+                         NOT NULL,
+    Menge        INTEGER NOT NULL,
+    Einzelpreis  REAL    NOT NULL,
+    Gesamtpreis  REAL    NOT NULL
+                         GENERATED ALWAYS AS (Menge * Einzelpreis) STORED,
+    Rabatt       REAL,
+    MwStSatz     INTEGER NOT NULL
+);
+
+
+-- Table: Bestellung
+DROP TABLE IF EXISTS Bestellung;
+
+CREATE TABLE IF NOT EXISTS Bestellung (
+    BestellungID     INTEGER PRIMARY KEY AUTOINCREMENT
+                             NOT NULL,
+    Datum            TEXT    NOT NULL,
+    KundenID         INTEGER REFERENCES Kunden (KundenID) ON DELETE CASCADE
+                                                          ON UPDATE CASCADE
+                             NOT NULL,
+    ProduktID        INTEGER REFERENCES Produkt (ProduktID) ON DELETE CASCADE
+                                                            ON UPDATE CASCADE
+                             NOT NULL,
+    MitarbeiterID    INTEGER REFERENCES Mitarbeiter (MitarbeiterID) ON DELETE CASCADE
+                                                                    ON UPDATE CASCADE,
+    Lieferdatum      TEXT    NOT NULL,
+    Status           TEXT,
+    Gesamtbetrag     REAL,
+    Lieferadresse    TEXT,
+    Rechnungsadresse TEXT
+);
+
 
 -- Table: Kategorie
 DROP TABLE IF EXISTS Kategorie;
@@ -32,6 +94,31 @@ CREATE TABLE IF NOT EXISTS Kunden (
     stadt         TEXT,
     Mobilenummer  TEXT,
     Telefonnummer TEXT
+);
+
+
+-- Table: Lieferant
+DROP TABLE IF EXISTS Lieferant;
+
+CREATE TABLE IF NOT EXISTS Lieferant (
+    LieferantID INTEGER    PRIMARY KEY AUTOINCREMENT
+                           NOT NULL,
+    Name        TEXT (255) NOT NULL,
+    Kontakt     TEXT (255),
+    Adresse     TEXT (255) NOT NULL
+);
+
+
+-- Table: Mitarbeiter
+DROP TABLE IF EXISTS Mitarbeiter;
+
+CREATE TABLE IF NOT EXISTS Mitarbeiter (
+    MitarbeiterID INTEGER PRIMARY KEY AUTOINCREMENT
+                          NOT NULL,
+    Vorname       TEXT    NOT NULL,
+    Nachname      TEXT    NOT NULL,
+    Beruf         TEXT,
+    Abteilung     TEXT
 );
 
 
